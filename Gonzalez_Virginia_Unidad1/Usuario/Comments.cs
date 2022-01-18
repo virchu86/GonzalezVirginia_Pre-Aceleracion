621581bb1ca02cd83900cb99d78fddf42fa95c43
virchu86 <virginia.noemi.gonzalez@gmail.com>
2022-01-18 20:17:17 -0300

    primera entrega

---
+++ Gonzalez_Virginia_Unidad1/Usuario/Comments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidad_1.Comments
+{
+    internal class Comments
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Comment { get; set; }
+
+        public int UsuarioId { get; set; }
+    }
+}
---
+++ Gonzalez_Virginia_Unidad1/Usuario/Posts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidad_1.Posts
+{
+    internal class Posts
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+        public int UsuarioId { get; set; }
+    }
+}
